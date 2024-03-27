fb03c319668e5c38cfff44a7a6d5a7aa8886624f
Soban Umar <sobanomr@gmail.com>
2024-03-27 06:11:04 +0500

    [responsive] Header + Footer

--- client/src/pages/Portfolio/Footer.jsx
+++ client/src/pages/Portfolio/Footer.jsx
@@ -7,8 +7,8 @@ import { FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
     <div>
-      <div className="flex gradient-background-horizontal text-white p-12 pb-20 justify-between">
-        <div className="w-[25%] flex flex-col gap-4">
+      <div className="flex gap-12 md:gap-0 flex-col md:flex-row gradient-background-horizontal text-white p-6 md:p-12 pb-20 justify-between">
+        <div className="md:w-[25%] flex flex-col gap-4">
           <div className="flex items-center">
             <div className="size-16">
               <img src={Logo} />
@@ -20,7 +20,7 @@ const Footer = () => {
             likes on your social accounts.
           </div>
         </div>
-        <div className="flex flex-col gap-2  w-[20%]">
+        <div className="flex flex-col gap-2  md:w-[20%]">
           <h1 className="text-2xl font-bold uppercase">Services</h1>
           <div>
             <h1 className="text-lg">Youtube Followers & Likes</h1>
@@ -30,7 +30,7 @@ const Footer = () => {
             <h1 className="text-lg">Google Views</h1>
           </div>
         </div>
-        <div className="flex flex-col gap-2 w-[15%]">
+        <div className="hidden md:flex flex-col gap-2 md:w-[15%]">
           <h1 className="text-2xl font-bold uppercase">quick links</h1>
           <div>
             <h1 className="text-lg">Home</h1>
@@ -39,17 +39,38 @@ const Footer = () => {
             <h1 className="text-lg">Login & Sign Up</h1>
           </div>
         </div>
-        <div className="flex flex-col gap-2 w-[18%]">
+        <div className="hidden md:flex flex-col gap-2 md:w-[18%]">
           <h1 className="text-2xl font-bold  uppercase">Follow us</h1>
           <div className="flex gap-4 ">
-            <FaFacebook size={40} />
-            <FaInstagram size={40} />
-            <FaYoutube size={40} />
-            <FaTwitter size={40} />
+            <FaFacebook className="size-5 md:size-10" />
+            <FaInstagram className="size-5 md:size-10" />
+            <FaYoutube className="size-5 md:size-10" />
+            <FaTwitter className="size-5 md:size-10" />
+          </div>
+        </div>
+
+        <div className="md:hidden flex gap-20">
+          <div className="flex flex-col gap-2 md:w-[15%]">
+            <h1 className="text-2xl font-bold uppercase">quick links</h1>
+            <div>
+              <h1 className="text-lg">Home</h1>
+              <h1 className="text-lg">Workers</h1>
+              <h1 className="text-lg">Buyers</h1>
+              <h1 className="text-lg">Login & Sign Up</h1>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 md:w-[18%]">
+            <h1 className="text-2xl font-bold  uppercase">Follow us</h1>
+            <div className="flex gap-4 ">
+              <FaFacebook className="size-5 md:size-10" />
+              <FaInstagram className="size-5 md:size-10" />
+              <FaYoutube className="size-5 md:size-10" />
+              <FaTwitter className="size-5 md:size-10" />
+            </div>
           </div>
         </div>
       </div>
-      <div className="w-full text-center py-3 font-bold text-xl">
+      <div className="w-full text-center py-3 font-bold text-lg md:text-xl">
         © Copyright 2024 Earn 4 Views LTD . All Rights Reserved.
       </div>
     </div>
--- client/src/pages/Portfolio/Header.jsx
+++ client/src/pages/Portfolio/Header.jsx
@@ -1,26 +1,59 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import logo from "../../assets/svg/main-logo.svg";
 import { FaUserCircle } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 
 const Header = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
-    <div className="gradient-background-horizontal flex justify-between px-8 py-2 items-center fixed w-full top-0 z-50">
-      <div>
-        <img src={logo} className="size-16" />
+    <div className="gradient-background-horizontal  py-1  fixed w-full top-0 z-50">
+      <div className="flex justify-between items-center px-4 md:px-8 md:py-2">
+        <div>
+          <img src={logo} className="size-14 md:size-16" />
+        </div>
+        <div className="md:gap-12 font-bold text-white text-lg hidden md:flex">
+          <Link to="/">Home</Link>
+          <Link to="/workers">Workers</Link>
+          <Link to="/buyers">Buyers</Link>
+        </div>
+        <div className="hidden md:flex">
+          <Link to="/login">
+            <button className="px-3 py-2 rounded-md bg-white font-bold flex gap-2 items-center">
+              <FaUserCircle size={24} />
+              Login/Signup
+            </button>
+          </Link>
+        </div>
+        <div
+          className="flex md:hidden"
+          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+        >
+          {!mobileMenuOpen ? (
+            <IoMenu color="white" size={33} />
+          ) : (
+            <IoClose color="white" size={33} />
+          )}
+        </div>
       </div>
-      <div className="flex gap-12 font-bold text-white text-lg">
-        <Link to="/">Home</Link>
-        <Link to="/workers">Workers</Link>
-        <Link to="/buyers">Buyers</Link>
-      </div>
-      <div>
-        <Link to="/login">
-          <button className="px-3 py-2 rounded-md bg-white font-bold flex gap-2 items-center">
-            <FaUserCircle size={24} />
-            Login/Signup
-          </button>
-        </Link>
-      </div>
+
+      {mobileMenuOpen && (
+        <div className="absolute md:hidden w-full font-bold text-white text-lg  ">
+          <div className="flex flex-col m-2 gradient-background-horizontal p-2 rounded-lg">
+            <div className="my-2">
+              <Link to="/">Home</Link>
+            </div>
+            <div className="my-2">
+              <Link to="/workers">Workers</Link>
+            </div>
+            <div className="my-2">
+              <Link to="/buyers">Buyers</Link>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
